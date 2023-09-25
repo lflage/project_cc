19db22d80a3332bf66d7bbaa88d920d6a303afac
lage.lucas <lufo01@dfki.de>
2023-09-25 14:49:34 +0200

    changed output prefixes, changed default to seq2seq

--- personaAI_processing.py
+++ personaAI_processing.py
@@ -11,14 +11,14 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-task', type=str,
                     choices=['clm','seq2seq'],
                     help='desire output format',
-                    default='clm')
+                    default='seq2seq')
 parser.add_argument('--out_dir', type=str, default= base_dir+"/datasets/processed/personaAI",
-                    help='Path to output file ')
+                    help='Path to output file')
 parser.add_argument('--in_file', type=str,
                     default=base_dir+'/datasets/raw/personachat_self_original.json',
                     help='Path to original dataset')
 parser.add_argument('--prefix', action='store_true',
-                    help='if passed, the task prefix is passed')                           
+                    help='if passed, the task prefix is passed')                     
 
 args = parser.parse_args()
 
@@ -46,25 +46,25 @@ def persona_samples(file, split='train'):
 
 if args.task=='seq2seq':
     # %%
-    with open("./datasets/processed/personaAI/train_seq2seq.json", 'w') as f:
+    with open(f"./datasets/processed/personaAI/train_{args.task}_prefix{args.prefix}.json", 'w') as f:
         for input_text, target in persona_samples(a,split='train'):
-            f.write(f"{prefix} {input_text} <answer> {target}".strip())
+            json.dump({'text': f"{prefix} {input_text}".strip(), "target": f"{target}"},f)
             f.write("\n")
     # %%
-    with open("./datasets/processed/personaAI/valid_seq2seq.json", 'w') as f:
+    with open(f"./datasets/processed/personaAI/validation_{args.task}_prefix{args.prefix}.json", 'w') as f:
         for input_text, target in persona_samples(a,split='valid'):
-            f.write(f"{prefix} {input_text} <answer> {target}".strip())
+            json.dump({'text': f"{prefix} {input_text}".strip(), "target": f"{target}"},f)
             f.write("\n")
 
 
 if args.task=='clm':
     # %%
-    with open("./datasets/processed/personaAI/train_clm.txt", 'w') as f:
+    with open(f"./datasets/processed/personaAI/train_{args.task}_prefix{args.prefix}.json", 'w') as f:
         for input_text, target in persona_samples(a,split='train'):
             f.write(input_text + " <answer> " + target)
             f.write("\n")
     # %%
-    with open("./datasets/processed/personaAI/valid_clm.txt", 'w') as f:
+    with open(f"./datasets/processed/personaAI/validation_{args.task}_prefix{args.prefix}.json", 'w') as f:
         for input_text, target in persona_samples(a,split='valid'):
             f.write(f"{prefix} {input_text} <answer> {target}".strip())
             f.write("\n")
