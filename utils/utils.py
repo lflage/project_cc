d047b07864b0260e50af6d14cc00373eb899e6ee
lage.lucas <lufo01@dfki.de>
2023-09-29 19:09:49 +0200

    fixed typos

--- utils/utils.py
+++ utils/utils.py
@@ -1,10 +1,9 @@
 example_list = ['Hello, how are you doing today?',
 "What's your name?", 
 "What do you like to do?",
-"what is you favourtite food?"]
+"what is your favorite food?"]
 
 persona = ["I'm Cockroach-Chan",
-"I'm based on an anime called gokicha",
-"I like eating breadcrumbs",
-"I like watching anime",
-"I live in the CoLi Building, in the University of Saarland"]
+"I'm a cockroach girl",
+"I live in the CoLi Building, in the University of Saarland",
+"My favorite food is breadcrumbs"]
